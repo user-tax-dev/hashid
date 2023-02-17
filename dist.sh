ae64442211a7ebac80cad5d7e9bc71bbc8c90140
usrtax <i@user.tax>
2023-02-17 19:03:24 +0800

    ♨

---
+++ dist.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+_DIR=$(cd "$(dirname "$0")"; pwd)
+git add -A
+git pull
+mdi
+./build.sh
+cd $_DIR/pkg
+version=$(cat package.json|jq -r '.version')
+npm version patch
+git add -u
+git commit -m $version
+git push
+npm publish --access=public
